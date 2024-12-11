c61e20c03ed65ed5c80a5e731c5f4964494a8c38
diegoberriogen <diego.berrio@genstudents.org>
2024-12-11 11:10:52 -0500

    ajuste task 1

--- task1.js
+++ task1.js
@@ -1,10 +1,3 @@
-// export function costCalculator(montoToltal) {
-//     const tarifaFija = 3;
-//     const porcentajeIntereses = 0.01;
-//     return resultado = montoTotal * porcentajeIntereses + tarifaFija;
-//     return resultado.toFixed(2);
-
-// }
 
 export function costCalculator(montoTransaccion) {
     const monto = parseFloat(montoTransaccion);
